36ecef0fa22d3b7a90ab38593d91b9b3749f4415
Austin West <korruptedanomaly@gmail.com>
2019-06-10 16:11:40 -0600

    Revert "Revert "Functioning flashlight with all black diamonds done""

--- Flashlight/Flashlight/ViewControllers/FlashlightViewController.swift
+++ Flashlight/Flashlight/ViewControllers/FlashlightViewController.swift
@@ -7,54 +7,147 @@
 //
 
 import UIKit
+import AVFoundation
 
 class FlashlightViewController: UIViewController {
-
+    
     @IBOutlet weak var flashlightToggleButton: UIButton!
     
-    
     var isOn: Bool = false
-    let turnOn: String = "Turn On"
-    let turnOff: String = "Turn Off"
-    
+    // let turnOn: String = "Turn On"
+    // let turnOff: String = "Turn Off"
+    override var preferredStatusBarStyle : UIStatusBarStyle {
+        if isOn == false {
+            return .lightContent
+        } else {
+            return .default
+        }
+    }
+        
     override func viewDidLoad() {
         super.viewDidLoad()
-
-       
+        
     }
     
-
-    @IBAction func flashlightToggleButtonTapped(_ sender: Any) {
+    //    Swipe right to toggle light
+    @IBAction func userHasSwipedRight(_ sender: UISwipeGestureRecognizer) {
         
         if isOn == false {
-           isOn = true
+            isOn = true
             self.view.backgroundColor = UIColor.white
             self.flashlightToggleButton.setTitle("Turn Off", for: .normal)
             flashlightToggleButton.setTitleColor(.black, for: .normal)
+            toggleTorch(on: true)
         } else {
-            if isOn == true {
             isOn = false
-                self.view.backgroundColor = UIColor.black
-                self.flashlightToggleButton.setTitle("Turn On", for: .normal)
-                flashlightToggleButton.setTitleColor(.white, for: .normal)
-            }
+            self.view.backgroundColor = UIColor.black
+            self.flashlightToggleButton.setTitle("Turn On", for: .normal)
+            flashlightToggleButton.setTitleColor(.white, for: .normal)
+            toggleTorch(on: false)
         }
+        self.setNeedsStatusBarAppearanceUpdate()
         
+        print("View was swiped right")
+    }
+    
+    //    swipe left to toggle light
+    @IBAction func userHasSwipedLeft(_ sender: UISwipeGestureRecognizer) {
+        if isOn == false {
+            isOn = true
+            self.view.backgroundColor = UIColor.white
+            self.flashlightToggleButton.setTitle("Turn Off", for: .normal)
+            flashlightToggleButton.setTitleColor(.black, for: .normal)
+            toggleTorch(on: true)
+        } else {
+            isOn = false
+            self.view.backgroundColor = UIColor.black
+            self.flashlightToggleButton.setTitle("Turn On", for: .normal)
+            flashlightToggleButton.setTitleColor(.white, for: .normal)
+            toggleTorch(on: false)
+        }
+        self.setNeedsStatusBarAppearanceUpdate()
         
+        print("View was swiped left")
+    }
+    
+    //    swipe up to toggle light
+    @IBAction func userHasSwipedUp(_ sender: UISwipeGestureRecognizer) {
+        if isOn == false {
+            isOn = true
+            self.view.backgroundColor = UIColor.white
+            self.flashlightToggleButton.setTitle("Turn Off", for: .normal)
+            flashlightToggleButton.setTitleColor(.black, for: .normal)
+            toggleTorch(on: true)
+        } else {
+            isOn = false
+            self.view.backgroundColor = UIColor.black
+            self.flashlightToggleButton.setTitle("Turn On", for: .normal)
+            flashlightToggleButton.setTitleColor(.white, for: .normal)
+            toggleTorch(on: false)
+        }
+        self.setNeedsStatusBarAppearanceUpdate()
+        
+        print("View was swiped up")
+    }
+    
+    // swipe down to toggle light
+    @IBAction func userHasSwipedDown(_ sender: UISwipeGestureRecognizer) {
+        if isOn == false {
+            isOn = true
+            self.view.backgroundColor = UIColor.white
+            self.flashlightToggleButton.setTitle("Turn Off", for: .normal)
+            flashlightToggleButton.setTitleColor(.black, for: .normal)
+            toggleTorch(on: true)
+        } else {
+            isOn = false
+            self.view.backgroundColor = UIColor.black
+            self.flashlightToggleButton.setTitle("Turn On", for: .normal)
+            flashlightToggleButton.setTitleColor(.white, for: .normal)
+            toggleTorch(on: false)
+        }
+        self.setNeedsStatusBarAppearanceUpdate()
+        
+        print("View was swiped down")
     }
     
     
+    /* @IBAction func flashlightToggleButtonTapped(_ sender: Any) {
+     
+     if isOn == false {
+     isOn = true
+     self.view.backgroundColor = UIColor.white
+     self.flashlightToggleButton.setTitle("Turn Off", for: .normal)
+     flashlightToggleButton.setTitleColor(.black, for: .normal)
+     toggleTorch(on: true)
+     } else {
+     isOn = false
+     self.view.backgroundColor = UIColor.black
+     self.flashlightToggleButton.setTitle("Turn On", for: .normal)
+     flashlightToggleButton.setTitleColor(.white, for: .normal)
+     toggleTorch(on: false)
+     }
+     self.setNeedsStatusBarAppearanceUpdate()
+     } */
     
-    
-    
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func toggleTorch(on: Bool) {
+        guard let device = AVCaptureDevice.default(for: .video) else { return }
+        
+        if device.hasTorch {
+            do {
+                try device.lockForConfiguration()
+                
+                if on == true {
+                    device.torchMode = .on
+                } else {
+                    device.torchMode = .off
+                }
+                
+                device.unlockForConfiguration()
+            } catch {
+                print("Torch could not be used")
+            }
+        } else {
+            print("Torch is not available")
+        }
     }
-    */
-
 }
